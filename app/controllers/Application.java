0217cdd780c2acaee5a43f216f1984027ab44b5a
Merve ÇAVDAR <mhndsmerve@hotmail.com>
2011-07-09 00:50:46 +0300

    test commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+import play.data.validation.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    public static void sayHello(@Required String myName) {
+        if(validation.hasErrors()) {
+            flash.error("Oops, please enter your name!");
+            index();
+        }
+        render(myName);
+    }
+
+}
